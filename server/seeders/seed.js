2d4fc94b9e3863072fcdb66b1ac4806a8e07a5db
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-09-08 22:56:14 -0400

    Deck collection now populated with cards

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,21 +1,16 @@
 const db = require('../config/connection');
 const { Card, Deck } = require('../models');
-const heartSeeds = require('./heartSeeds.json');
-const diamondSeeds = require('./diamondSeeds.json');
-const spadeSeeds = require('./spadeSeeds.json');
-const clubSeeds = require('./clubSeeds.json');
+const cardSeeds = require('./cardSeeds.json');
 
 db.once('open', async () => {
   try {
     await Card.deleteMany({});
-    await Card.create(heartSeeds);
-    console.log('hearts');
-    await Card.create(diamondSeeds);
-    console.log('diamonds');
-    await Card.create(spadeSeeds);
-    console.log('spades');
-    await Card.create(clubSeeds);
-    console.log('clubs');
+    await Deck.deleteMany({});
+    const cards = await Card.create(cardSeeds);
+    await Deck.create({
+      cards: cards
+    });
+
   } catch (err) {
     console.error(err);
     process.exit(1);
