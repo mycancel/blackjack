3c9c13802bf435c503c55163d2a1714fb3f38a3d
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-09-08 22:41:34 -0400

    created seed data for the rest of the suits

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,11 +1,21 @@
 const db = require('../config/connection');
 const { Card, Deck } = require('../models');
 const heartSeeds = require('./heartSeeds.json');
+const diamondSeeds = require('./diamondSeeds.json');
+const spadeSeeds = require('./spadeSeeds.json');
+const clubSeeds = require('./clubSeeds.json');
 
 db.once('open', async () => {
   try {
     await Card.deleteMany({});
     await Card.create(heartSeeds);
+    console.log('hearts');
+    await Card.create(diamondSeeds);
+    console.log('diamonds');
+    await Card.create(spadeSeeds);
+    console.log('spades');
+    await Card.create(clubSeeds);
+    console.log('clubs');
   } catch (err) {
     console.error(err);
     process.exit(1);
