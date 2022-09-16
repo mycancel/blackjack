28b04fa93752cd41e7d00db9c7608fccb4ee13e2
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-09-15 23:44:38 -0400

    added main tag and Hand component; created state to setCards in hand

---
+++ client/src/components/Hand.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hand = ({ cards }) => {
+  return (
+    <div>Hand</div>
+  )
+}
+
+export default Hand
--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -1,13 +1,18 @@
-import React from 'react'
-import { drawNumber } from '../utils/helpers';
+import React, { useState } from 'react'
+import { drawNumber, startGame } from '../utils/helpers';
+import Hand from '../components/Hand';
 
 const Game = () => { 
+  const [cards, setCards] = useState(startGame());
+
   return (
     <>
       {/* TODO: Navigation bar for going back to the home page */}
+      <button onClick={() => setCards(drawNumber())}>Deck</button>
       {/* TODO: Add main element with hand component and dealer component */}
-
-      <button onClick={drawNumber()}>Deck</button>
+      <main>
+        <Hand cards={cards}/>
+      </main>
     </>
   )
 }
