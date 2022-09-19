4c19135b8f7b292c41377f19a9e4eb5822215e6e
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-09-19 14:23:46 -0400

    edited state names; created dealerDraw and dealerHand states; created Dealer component; started new useEffect for dealerDraw

---
+++ client/src/components/Dealer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Dealer = ({ hand }) => {
+  console.log(hand);
+  return (
+    <div>Dealer</div>
+  )
+}
+
+export default Dealer
--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -1,38 +1,54 @@
 import React, { useEffect, useState } from 'react'
 import { drawNumber, startGame } from '../utils/helpers';
 import Hand from '../components/Hand';
+import Dealer from '../components/Dealer';
 
 const Game = () => { 
-  // card will be used to represent the dealt out card(s)
-  const [card, setCards] = useState(startGame());
+  // playerDraw will be used to represent the card(s) dealt to the player
+  const [playerDraw, setPlayerDraw] = useState(startGame());
+  // dealerDraw will be used to represent the card(s) dealt to the dealer
+  const [dealerDraw, setDealerDraw] = useState(startGame());
   // The hand will be what the player is holding (array of integers)
   const [hand, setHand] = useState([]);
+  // The dealer will be what the dealer is holding (array of integers)
+  const [dealerHand, setDealer]= useState([]);
   
-  // When the value of card changes, localStorage is updated
+  // When the value of playerDraw changes, localStorage is updated
   useEffect(() => {
     // 'player' array is recieved from localStorage
     const player = JSON.parse(localStorage.getItem('player')) || [];
-    // If card is an object,
-    if (typeof card === 'object') {
-      // each element of the object is added to the 'player' array
-      card.forEach((num) => player.push(num));
-    // If the card is not an object, it will be a number.
+    // If playerDraw is an object, each element of the object is added to the 'player' array
+    if (typeof playerDraw === 'object') playerDraw.forEach((num) => player.push(num));
+    // If the playerDraw is not an object, it will be a number.
     // So that is added to the 'player' array
-    } else player.push(card);
+    else player.push(playerDraw);
     // Finally, localStorage is updated with the new 'player' array
     localStorage.setItem('player', JSON.stringify(player));
     // and the hand is set to the updated array from localStorage
     setHand(JSON.parse(localStorage.getItem('player')) || []);
-  }, [card]);
+  }, [playerDraw]);
+
+  // When the value of dealerDraw changes, localStorage is updated in a similar way as 'player'
+  // TODO: Fix Local Storage for Dealer
+  useEffect(() => {
+    const dealer = JSON.parse(localStorage.getItem('dealer')) || [];
+    if (typeof dealerDraw === 'object') dealer.forEach((num) => dealer.push(num));
+    else dealer.push(dealerDraw);
+    console.log(dealer);
+    localStorage.setItem('dealer', JSON.stringify(dealer));
+    setDealer(JSON.parse(localStorage.getItem('dealer')));
+  }, [dealerDraw])
 
   return (
     <>
       {/* TODO: Navigation bar for going back to the home page */}
-      <button onClick={() => setCards(drawNumber())}>Deck</button>
+      <button onClick={() => setPlayerDraw(drawNumber())}>Deck</button>
       {/* TODO: Add main element with Hand component and dealer component */}
       <main>
+        <Dealer hand={dealerHand}/>
         <Hand hand={hand} />
       </main>
+      {/* TODO: Footer that tallies up the value of points from the player */}
     </>
   )
 }
