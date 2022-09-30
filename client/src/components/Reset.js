57223ce3ce31cb0e73ca7f6ad394b27931213b5e
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-09-29 21:59:34 -0400

    styled reset modal and content

--- client/src/components/Reset.js
+++ client/src/components/Reset.js
@@ -4,43 +4,53 @@ import ResetBtn from './ResetBtn';
 const Reset = ({dealer, player}) => {
   if (player > 21) {
     return (
-      <div>
-        Dealer Won!
-        <ResetBtn />
+      <div className='resetModal'>
+        <div className='resetModalContent'>
+          <h2>Dealer Won!</h2>
+          <ResetBtn />
+        </div>
       </div>
     )
   } else if (dealer > 21) {
     return (
-      <div>
-        Player Won!
-        <ResetBtn />
+      <div className='resetModal'>
+        <div className='resetModalContent'>
+          <h2>Player Won!</h2>
+          <ResetBtn />
+        </div>
       </div>
     )
   } else if (dealer === player) {
     return (
-      <div>
-        Tie!
-        <ResetBtn />
+      <div className='resetModal'>
+        <div className='resetModalContent'>
+          <h2>Tie!</h2>
+          <ResetBtn />
+        </div>
       </div>
     )
   } else if (dealer > player && dealer <= 21) {
     return (
-      <div>
-        Dealer Won!
-        <ResetBtn />
+      <div className='resetModal'>
+        <div className='resetModalContent'>
+          <h2>Dealer Won!</h2>
+          <ResetBtn />
+        </div>
       </div>
     )
   } else if (player > dealer && player <= 21) {
     return (
-      <div>
-        Player Won!
-        <ResetBtn />
+      <div className='resetModal'>
+        <div className='resetModalContent'>
+          <h2>Player Won!</h2>
+          <ResetBtn />
+        </div>
       </div>
     )
   } else {
     return (
       <div>
-        Reset
+        Reset Component
       </div>
     )
   }
